18ccbd702647feb4b0f799963a025b1fd9c34c1e
DESKTOP-3GS0CMR\Yatahagane <achmadfm18@gmail.com>
2019-07-22 07:10:55 +0800

    perubahan link IG

--- application/views/depan/social.php
+++ application/views/depan/social.php
@@ -1,3 +1,3 @@
 <li class="social-icons-facebook"><a href="https://www.facebook.com/bosowabinainsani/" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-<li class="social-icons-instagram"><a href="https://www.instagram.com/sbbi.inter/" target="_blank" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+<li class="social-icons-instagram"><a href="https://www.instagram.com/bosowabinainsani/" target="_blank" title="Instagram"><i class="fa fa-instagram"></i></a></li>
 <li class="social-icons-youtube"><a href="https://www.youtube.com/channel/UCtQZomwlcfcTlUAF_YKyHiw" target="_blank" title="Youtube"><i class="fa fa-youtube"></i></a></li>
