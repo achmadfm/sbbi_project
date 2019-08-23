0f354492ceba5d2bc1e18790694d2c2de19b5826
DESKTOP-3GS0CMR\Yatahagane <achmadfm18@gmail.com>
2019-08-23 21:57:16 +0800

    aduh

--- application/views/depan/nav_menu.php
+++ application/views/depan/nav_menu.php
@@ -30,7 +30,7 @@
           <li><a class="dropdown-item" href="<?php echo base_url('Sbbi/info_pg')?>">Play Group</a></li>
       </ul>
     </li>
-    <li class="dropdown <?php echo activate_menu('berita'); echo activate_menu('pengumuman'); echo activate_menu('insight_reading'); echo activate_menu('newsletter');?>">
+    <li class="dropdown <?php echo activate_menu('berita'); echo activate_menu('pengumuman'); echo activate_menu('insight_reading'); echo activate_menu('emagazine');?>">
       <a class="dropdown-toggle" href="#">
         Info
       </a>
@@ -38,7 +38,7 @@
         <li><a class="dropdown-item" href="<?php echo base_url('berita')?>">Berita</a></li>
         <li><a class="dropdown-item" href="<?php echo base_url('pengumuman')?>">Pengumuman | Acara</a></li>
         <li><a class="dropdown-item" href="<?php echo base_url('insight_reading')?>">Insight reading</a></li>
-        <li><a class="dropdown-item" href="<?php echo base_url('newsletter')?>">NewsLetter</a></li>
+        <li><a class="dropdown-item" href="<?php echo base_url('emagazine')?>">E-Magazine</a></li>
       </ul>
     </li>
     <li class="<?php echo activate_menu('prestasi_siswa');?>">
