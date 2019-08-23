99accdfb9e94e4438645c96d9452ba94f2ef789d
DESKTOP-3GS0CMR\Yatahagane <achmadfm18@gmail.com>
2019-08-23 21:52:43 +0800

    sedikit

--- application/views/admin/v_dashboard.php
+++ application/views/admin/v_dashboard.php
@@ -199,7 +199,7 @@
                                 <table class="table">
                                     <tbody>
                                       <?php
-                                          $query=$this->db->query("SELECT * FROM tbl_tulisan ORDER BY tulisan_views DESC");
+                                          $query=$this->db->query("SELECT * FROM tbl_tulisan ORDER BY tulisan_views DESC limit 5");
                                           foreach ($query->result_array() as $i) :
                                               $tulisan_id=$i['tulisan_id'];
                                               $tulisan_judul=$i['tulisan_judul'];
