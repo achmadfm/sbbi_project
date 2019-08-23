78c296be7fc4d71516fb62ee22ea660a6af82846
DESKTOP-3GS0CMR\Yatahagane <achmadfm18@gmail.com>
2019-08-23 15:21:28 +0800

    anu ji

--- template/js/instagram/home.js
+++ template/js/instagram/home.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
         limit: 9,
         sortBy: 'most-recent',
 		resolution: 'thumbnail',
-		template: '<li><a href="{{link}}"><span class="img-thumbnail d-block"><img class="img-fluid" width="70" height="70" src="{{image}}"></span></a><br><p><i class="fa fa-heart"></i> {{likes}}  <i class="fa fa-comment"></i> {{comments}}</p></li>'
+		template: '<li><a href="{{link}}" target="_new"><span class="img-thumbnail d-block"><img class="img-fluid" width="70" height="70" src="{{image}}"></span></a><br><p><i class="fa fa-heart"></i> {{likes}}  <i class="fa fa-comment"></i> {{comments}}</p></li>'
 	});
 	feed.run();
 });
