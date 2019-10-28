971c0280d46def7f013290b9922b40e364cc8076
DESKTOP-3GS0CMR\Yatahagane <achmadfm18@gmail.com>
2019-10-28 08:34:41 +0800

    ada br nya

--- application/views/depan/v_info_pg.php
+++ application/views/depan/v_info_pg.php
@@ -301,7 +301,7 @@
 										<a href="http://bit.ly/Register_BSM" target="_blank" class="btn btn-md btn-primary">Daftar? Klik disini!</a>
 									</div> -->
 									<div class="call-to-action-content align-left pb-md mb-xl ml-none">
-										<h2 class="text-color-light mb-none">Pendaftaran Siswa Baru <br> Bosowa Bina Insani Bogor Tahun Ajaran 2019/2020 <strong>Sudah DiBuka</strong></h2>
+										<h2 class="text-color-light mb-none">Pendaftaran Siswa Baru <br> Bosowa Bina Insani Bogor Tahun Ajaran 2019/2020 <br> <strong>Sudah DiBuka</strong></h2>
 										<br>
 										<a href="http://bit.ly/Register_BSM" target="_blank" class="btn btn-md btn-primary">Daftar? Klik disini!</a>
 									</div>
