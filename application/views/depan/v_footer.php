9e2bd5c3f8bd9a439d47f2a4b78b06484905fc29
DESKTOP-3GS0CMR\Yatahagane <achmadfm18@gmail.com>
2020-01-09 15:13:53 +0800

    tambah fitur download brosur

--- application/views/depan/v_footer.php
+++ application/views/depan/v_footer.php
@@ -6,18 +6,17 @@
 			</div>
 			<div class="col-lg-3">
 				<div class="contact-details">
-				    <br>
     			        <h4><strong>PG - TK - SD</strong></h4>
     					<a target="_blank" href="https://www.google.com/maps/place/Bosowa+Bina+Insani/@-6.5583025,106.7852204,16z/data=!4m5!3m4!1s0x2e69c46e2834b39d:0xabfc5661d9f99759!8m2!3d-6.5567943!4d106.7854672"><p><i class="fa fa-map-marker"></i> <strong></strong> Jl. Seremped Wetan, Sukadamai, Kec. Tanah Sereal, Kota Bogor, Jawa Barat 16164</p></a>
-    			        
 					<br>
 					    <h4><strong>SMP - SMA</strong></h4>
 						<a target="_blank" href="https://www.google.com/maps/place/Bosowa+Bina+Insani/@-6.5583025,106.7852204,16z/data=!4m5!3m4!1s0x2e69c46e2834b39d:0xabfc5661d9f99759!8m2!3d-6.5567943!4d106.7854672"><p><i class="fa fa-map-marker"></i> <strong></strong> Jl. Seremped Wetan, Sukadamai, Kec. Tanah Sereal, Kota Bogor, Jawa Barat 16164</p></a>
 					    
 				</div>
 			</div>
-			<div class="col-sm-1">
-			    
+			<div class="col-sm-3">
+			<h4><strong>Download Brosur</strong></h4>
+					<a href="#" data-toggle="modal" data-target="#defaultModal"><img src="<?php echo base_url('template/sbbid.png')?>" width="140px" height="80px" alt="download"></a>
 			</div>
 			<div class="col-lg-4">
 				<h5 class="mb-2">Call Us / Contact Persons</h5>
@@ -48,3 +47,82 @@
 		</div>
 	</div>
 </footer>
+
+<!-- Modals SMP-SMA -->
+<div class="modal fade" id="defaultModal" tabindex="-1" role="dialog" aria-labelledby="defaultModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title" id="defaultModalLabel">Download Brosur</h4>
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+			</div>
+			<div class="modal-body">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>
+							#
+						</th>
+						<th>
+							Unit
+						</th>
+						<th>
+							Files
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="default">
+						<th scope="row">
+							1
+						</th>
+						<td>
+							Brosur SMA Bosowa Bina Insani Bogor
+						</td>
+						<td>
+							<a class="btn btn-sm btn-default" href="<?php echo base_url('template/brosur/brosur_SMA_SBBI.pdf')?>" download><i class="fa fa-download"></i> Download File</a>
+						</td>
+					</tr>
+					<tr class="default">
+						<th scope="row">
+							2
+						</th>
+						<td>
+							Brosur SMP Bosowa Bina Insani Bogor
+						</td>
+						<td>
+							<a class="btn btn-sm btn-default" href="<?php echo base_url('template/brosur/brosur_SMP_SBBI.pdf')?>" download><i class="fa fa-download"></i> Download File</a>
+						</td>
+					</tr>
+					<tr class="default">
+						<th scope="row">
+							3
+						</th>
+						<td>
+							Brosur SD Bosowa Bina Insani Bogor
+						</td>
+						<td>
+							<a class="btn btn-sm btn-default" href="<?php echo base_url('template/brosur/brosur_SD_SBBI.pdf')?>" download><i class="fa fa-download"></i> Download File</a>
+						</td>
+					</tr>
+					<tr class="default">
+						<th scope="row">
+							4
+						</th>
+						<td>
+							Brosur PG-TK Bosowa Bina Insani Bogor
+						</td>
+						<td>
+							<a class="btn btn-sm btn-default" href="<?php echo base_url('template/brosur/brosur_TK_SBBI.pdf')?>" download><i class="fa fa-download"></i> Download File</a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- END OF MODALS SMP-SMA -->
